c43edc674d1a7a22f495c16e85da1632b1b8e1d7
mazin0eg <mazin.zougui@gmail.com>
2025-09-25 00:01:23 +0100

    larchitecture de proget nutritionnel est faite

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { render } from "ejs";
+import express from "express";
+import { join } from "path";
+import dotenv from "dotenv";
+import router from './router/router.js'
+
+const app =  express()
+
+dotenv.config()
+const PORT = process.env.PORT
+
+
+app.set('view engine' , 'ejs')
+app.set('views' , './src/view')
+app.use(express.static("public"));
+app.use(router)
+app.listen(PORT, () =>{
+   console.log("server runed")
+ })
