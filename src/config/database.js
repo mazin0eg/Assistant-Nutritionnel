f54de158fdab85dbbac369ab4e69fde7aea332eb
mazin0eg <mazin.zougui@gmail.com>
2025-09-25 16:18:16 +0100

    la connection de la base de donne est faite

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+// db.js
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,     
+    user: process.env.DB_USER,         
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE, 
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log("connection makhdamach mea db",err)
+        return
+    }
+    console.log('connection khdama mea db ');
+})
+
+export default connection;
