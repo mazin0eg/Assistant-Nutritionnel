d5c8d3359b629b727c709acaa8cdd69bb9ab7604
oussamaamou <amoussdark@gmail.com>
2025-09-26 02:37:20 +0100

    fix: amélioration de la configuration et du script SQL de la base de données

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,14 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,     
-    user: process.env.DB_USER,         
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME, 
+const db = await mysql.createPool({
+  host: process.env.DB_HOST,     
+  user: process.env.DB_USER,         
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME, 
+  waitForConnections: true,
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log("connection makhdamach mea db",err)
-        return
-    }
-    console.log('connection khdama mea db ');
-})
-
-export default connection;
+export default db;
