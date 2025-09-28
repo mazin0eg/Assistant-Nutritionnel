8a27967c67101f4fe0bc1c4343b571f2e6857836
oussamaamou <amoussdark@gmail.com>
2025-09-28 09:55:50 +0100

    refactor: suppression du 'await' inutile dans la connexion à la base de données

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = await mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,     
   user: process.env.DB_USER,         
   password: process.env.DB_PASSWORD, 
