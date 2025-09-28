36fee0b77dbddb9b50c8ab10b483243918520da1
oussamaamou <amoussdark@gmail.com>
2025-09-28 09:57:01 +0100

    feat: ajout des validations (express-validator) et du middleware de vérification sur les routes d’authentification

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { getRegister, getLogin, postRegister, postLogin, postLogout } from '../controllers/auth.controller.js';
+import { registerValidator, loginValidator } from '../validators/auth.validators.js';
+import { validate } from '../middlewares/validate.js';
 
 const r = Router();
 
 r.get('/register', getRegister);
-r.post('/register', postRegister);
-
 r.get('/login', getLogin);
-r.post('/login', postLogin);
+
+r.post('/register', registerValidator, validate('auth/register.ejs'), postRegister);
+r.post('/login', loginValidator, validate('auth/login.ejs'), postLogin);
 
 r.post('/logout', postLogout);
 
