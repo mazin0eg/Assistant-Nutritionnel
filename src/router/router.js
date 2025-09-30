7e43b4229779724037f54f1f0257b52c851e671c
oussamaamou <amoussdark@gmail.com>
2025-09-30 15:26:37 +0100

    feat: ajout d’une route vers l’interface d’analyse des repas

--- src/router/router.js
+++ src/router/router.js
@@ -1,20 +1,13 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.js';
-import db from '../config/database.js';
+import { getAnalyse } from '../controllers/meal.controller.js';
 
 const r = Router();
 
 r.get('/', auth, (req, res) => {
   res.render('index.ejs', { user: req.session.user });
+  r.get('/meal-analyse', getAnalyse);
 });
 
-r.get('/health/db', async (_req, res, next) => {
-  try {
-    const [rows] = await db.execute('SELECT 1 AS ok');
-    res.json(rows[0]); 
-  } catch (e) { 
-    next(e); 
-  }
-});
 
 export default r;
